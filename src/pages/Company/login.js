e8510c04b22da6c11cea5f0c21833bcb31c0663f
MDung0807 <77951587+MDung0807@users.noreply.github.com>
2024-06-23 14:28:35 +0700

    reload after login event

--- src/pages/Company/login.js
+++ src/pages/Company/login.js
@@ -51,7 +51,7 @@ const CompanyLogin = () => {
                 localStorage.setItem('avatar', response.data.avatar);
                 notifySuccess()
                 setTimeout(() => {
-                    navigate('/company/dashboard');
+                    window.location.href = '/company/dashboard';
                 }, 1500);
             }
             else {
